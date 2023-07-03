989f47c939a92ab3dfc2f007f65a00386086e615
Haonan Peng <38589727+HaonanPeng@users.noreply.github.com>
2023-07-03 11:38:35 -0700

    Add files via upload

---
+++ python_controller/run_r2_random_joint_movement_with_force.py
@@ -0,0 +1,276 @@
+"""
+Raven 2 Control - Control software for the Raven II robot
+Copyright (C) 2005-2022  Andrew Lewis, Yun-Hsuan Su, Haonan Peng, Blake Hannaford,
+and the University of Washington BioRobotics Laboratory
+This file is part of Raven 2 Control.
+Raven 2 Control is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+Raven 2 Control is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+You should have received a copy of the GNU Lesser General Public License
+along with Raven 2 Control.  If not, see <http://www.gnu.org/licenses/>.
+raven_keyboard_controller.py
+Python controller for RAVEN II using CRTK API
+date Jan 10, 2022
+author Haonan Peng, Yun-Hsuan Su, Andrew Lewis, 
+"""
+
+import time
+#import utils
+import rospy
+import numpy as np
+import raven_py_controller
+import sensor_msgs.msg
+
+run_time = 1200
+
+switch_smooth_factor = 1000.0 #[IMPT]: This is the smooth factor, it must be positive, and non-zero. Increase this factor will decrease the acceleration of joints after switching direction
+
+Deg2Rad = np.pi / 180.0
+Rad2Deg = 180.0 / np.pi
+
+# joint limits in degree and meter(joint 3), this is the soft limit of this controller
+limit_joint_1 = np.array([20.0, 60.0]) * Deg2Rad
+limit_joint_2 = np.array([55.0, 110.0]) * Deg2Rad
+limit_joint_3 = np.array([0.33, 0.42])
+
+#velocity_joint_1 = 3 # degree/s
+#velocity_joint_2 = 3 # degree/s
+#velocity_joint_3 = 0.01 # m/s
+
+# This will be the range of how joint target can be changed 
+limit_range_joint_1 = np.array([5, -5]) * Deg2Rad
+limit_range_joint_2 = np.array([10, -10]) * Deg2Rad
+limit_range_joint_3 = np.array([0.02, -0.02])
+
+vel_limit_joint_1 = np.array([1.091, 3]) * Deg2Rad
+vel_limit_joint_2 = np.array([1.5, 4]) * Deg2Rad
+vel_limit_joint_3 = np.array([0.0025, 0.007])
+#vel_limit_joint_1 = np.array([1.091, 2.182]) * Deg2Rad
+#vel_limit_joint_2 = np.array([1.5, 3]) * Deg2Rad
+#vel_limit_joint_3 = np.array([0.0025, 0.005])
+
+rospy.init_node('raven_randonm_movement', anonymous=True)
+
+r2py_ctl = raven_py_controller.raven2_py_controller(name_space = ' ', robot_name = 'arm1', grasper_name = 'grasp1')
+force_cmd_publisher = rospy.Publisher('force_cmd', sensor_msgs.msg.JointState, latch = True, queue_size = 1)  # create torque cmd publisher
+r2py_ctl.pub_state_command('resume')
+
+# randomly choose the initial direction of the movement
+dir_1 = np.sign(np.random.randn())
+dir_2 = np.sign(np.random.randn())
+dir_3 = np.sign(np.random.randn())
+
+# Set the initial movement target for each joint
+target_joint_1 = limit_joint_1 + np.random.rand() * limit_range_joint_1
+target_joint_2 = limit_joint_2 + np.random.rand() * limit_range_joint_2
+target_joint_3 = limit_joint_3 + np.random.rand() * limit_range_joint_3
+
+#Set initial joint velocity
+vel_joint_1 = np.random.uniform(vel_limit_joint_1[0], vel_limit_joint_1[1])
+vel_joint_2 = np.random.uniform(vel_limit_joint_2[0], vel_limit_joint_2[1])
+vel_joint_3 = np.random.uniform(vel_limit_joint_3[0], vel_limit_joint_3[1])
+
+# Set joint switch to 0
+after_switching_joint_1 = 0.0
+after_switching_joint_2 = 0.0
+after_switching_joint_3 = 0.0
+
+#-----------------------------------------------------------------------------------------------------------------------
+# joint limits in degree and meter(joint 3), this is the soft limit of this controller
+#limit_force_x = np.array([-2.8, 2.8]) 
+#limit_force_y = np.array([-2.8, 2.8]) 
+#limit_force_z = np.array([-2.8, 2.8])
+
+limit_force_x = np.array([-1.5, 1.5]) 
+limit_force_y = np.array([-1.5, 1.5]) 
+limit_force_z = np.array([-1.5, 1.5])
+
+#velocity_force_x = 3 # degree/s
+#velocity_force_y = 3 # degree/s
+#velocity_force_z = 0.01 # m/s
+
+# This will be the range of how joint target can be changed 
+limit_range_force_x = np.array([0.5, -0.5])
+limit_range_force_y = np.array([0.5, -0.5])
+limit_range_force_z = np.array([0.5, -0.5])
+
+vel_limit_force_x = np.array([0.1, 0.3])
+vel_limit_force_y = np.array([0.1, 0.3]) 
+vel_limit_force_z = np.array([0.1, 0.3])
+
+# randomly choose the initial direction of the movement
+dir_force_x = np.sign(np.random.randn())
+dir_force_y = np.sign(np.random.randn())
+dir_force_z = np.sign(np.random.randn())
+
+# Set the initial movement target for each joint
+target_force_x = limit_force_x + np.random.rand() * limit_range_force_x
+target_force_y = limit_force_y + np.random.rand() * limit_range_force_y
+target_force_z = limit_force_z + np.random.rand() * limit_range_force_z
+
+#Set initial joint velocity
+vel_force_x = np.random.uniform(vel_limit_force_x[0], vel_limit_force_x[1])
+vel_force_y = np.random.uniform(vel_limit_force_y[0], vel_limit_force_y[1])
+vel_force_z = np.random.uniform(vel_limit_force_z[0], vel_limit_force_z[1])
+
+# Set joint switch to 0
+after_switching_force_x = 0.0
+after_switching_force_y = 0.0
+after_switching_force_z = 0.0
+
+cur_force_x = 0
+cur_force_y = 0
+cur_force_z = 0
+
+
+#-----------------------------------------------------------------------------------------------------------------------
+
+start_time = time.time()
+
+while time.time() - start_time  <= run_time:
+    try:
+        cur_jpos_1 = r2py_ctl.measured_jpos[0]
+        cur_jpos_2 = r2py_ctl.measured_jpos[1]
+        cur_jpos_3 = r2py_ctl.measured_jpos[2]
+    except:
+        print('waiting for the measure_jp from CRTK')
+        continue
+    
+    # swith the direction after reaching target
+    if (dir_1 > 0) & (cur_jpos_1 > target_joint_1[1]):
+        dir_1 = dir_1 * -1
+        target_joint_1 = limit_joint_1 + np.random.rand() * limit_range_joint_1 # randomly change target
+        vel_joint_1 = np.random.uniform(vel_limit_joint_1[0], vel_limit_joint_1[1]) # randomly change velocity   
+        after_switching_joint_1 = 0.0   
+    if (dir_1 < 0) & (cur_jpos_1 < target_joint_1[0]):
+        dir_1 = dir_1 * -1
+        target_joint_1 = limit_joint_1 + np.random.rand() * limit_range_joint_1 # randomly change target
+        vel_joint_1 = np.random.uniform(vel_limit_joint_1[0], vel_limit_joint_1[1]) # randomly change velocity
+        after_switching_joint_1 = 0.0  
+
+    if (dir_2 > 0) & (cur_jpos_2 > target_joint_2[1]):
+        dir_2 = dir_2 * -1
+        target_joint_2 = limit_joint_2 + np.random.rand() * limit_range_joint_2 # randomly change target
+        vel_joint_2 = np.random.uniform(vel_limit_joint_2[0], vel_limit_joint_2[1]) # randomly change velocity
+        after_switching_joint_2 = 0.0  
+    if (dir_2 < 0) & (cur_jpos_2 < target_joint_2[0]):
+        dir_2 = dir_2 * -1
+        target_joint_2 = limit_joint_2 + np.random.rand() * limit_range_joint_2 # randomly change target
+        vel_joint_2 = np.random.uniform(vel_limit_joint_2[0], vel_limit_joint_2[1]) # randomly change velocity
+        after_switching_joint_2 = 0.0  
+        
+    if (dir_3 > 0) & (cur_jpos_3 > target_joint_3[1]):
+        dir_3 = dir_3 * -1
+        target_joint_3 = limit_joint_3 + np.random.rand() * limit_range_joint_3 # randomly change target
+        vel_joint_3 = np.random.uniform(vel_limit_joint_3[0], vel_limit_joint_3[1]) # randomly change velocity
+        after_switching_joint_3 = 0.0  
+    if (dir_3 < 0) & (cur_jpos_3 < target_joint_3[0]):
+        dir_3 = dir_3 * -1
+        target_joint_3 = limit_joint_3 + np.random.rand() * limit_range_joint_3 # randomly change target
+        vel_joint_3 = np.random.uniform(vel_limit_joint_3[0], vel_limit_joint_3[1]) # randomly change velocity
+        after_switching_joint_3 = 0.0  
+
+            
+    cmd = np.zeros((16))
+    if dir_1 > 0:
+        cmd[1] = dir_1 * np.clip(np.abs(target_joint_1[1] - cur_jpos_1), 0.2*Deg2Rad, vel_joint_1) * 1e-3 * np.clip(after_switching_joint_1/(200+switch_smooth_factor),0,1)
+    else:
+        cmd[1] = dir_1 * np.clip(np.abs(target_joint_1[0] - cur_jpos_1), 0.2*Deg2Rad, vel_joint_1) * 1e-3 * np.clip(after_switching_joint_1/(200+switch_smooth_factor),0,1)
+
+    if dir_2 > 0:
+        cmd[2] = dir_2 * np.clip(np.abs(target_joint_2[1] - cur_jpos_2), 0.2*Deg2Rad, vel_joint_2) * 1e-3 * np.clip(after_switching_joint_2/(200+switch_smooth_factor),0,1)
+    else:
+        cmd[2] = dir_2 * np.clip(np.abs(target_joint_2[0] - cur_jpos_2), 0.2*Deg2Rad, vel_joint_2) * 1e-3 * np.clip(after_switching_joint_2/(200+switch_smooth_factor),0,1)
+
+    if dir_3 > 0:
+        cmd[3] = dir_3 * np.clip(np.abs(target_joint_3[1] - cur_jpos_3), 0.001, vel_joint_3) * 1e-3 * np.clip(after_switching_joint_3/(200+switch_smooth_factor),0,1)
+    else:
+        cmd[3] = dir_3 * np.clip(np.abs(target_joint_3[0] - cur_jpos_3), 0.001, vel_joint_3) * 1e-3 * np.clip(after_switching_joint_3/(200+switch_smooth_factor),0,1)
+
+    print('------------------------------')
+    #print(traj_len)
+    print(int(time.time() - start_time))
+    print(cmd[1] * Rad2Deg *1000)
+    print(cmd[2] * Rad2Deg *1000)
+    print(cmd[3] *1000)
+    # cmd[1] = dir_1 * vel_joint_1 * 1e-3  # old code
+    # cmd[2] = dir_2 * vel_joint_2 * 1e-3  # old code
+    # cmd[3] = dir_3 * vel_joint_3 * 1e-3  # old code
+    r2py_ctl.pub_jr_command(cmd) 
+
+    after_switching_joint_1 += 1
+    #print(after_switching_joint_1)
+    after_switching_joint_2 += 1
+    after_switching_joint_3 += 1
+
+#-----------------------------------------------------------------------------------------------------------------------------------------------------
+    # swith the direction after reaching target
+    if (dir_force_x > 0) & (cur_force_x > target_force_x[1]):
+        dir_force_x = dir_force_x * -1
+        target_force_x = limit_force_x + np.random.rand() * limit_range_force_x # randomly change target
+        vel_force_x = np.random.uniform(vel_limit_force_x[0], vel_limit_force_x[1]) # randomly change velocity   
+        after_switching_force_x = 0.0   
+    if (dir_force_x < 0) & (cur_force_x < target_force_x[0]):
+        dir_force_x = dir_force_x * -1
+        target_force_x = limit_force_x + np.random.rand() * limit_range_force_x # randomly change target
+        vel_force_x = np.random.uniform(vel_limit_force_x[0], vel_limit_force_x[1]) # randomly change velocity
+        after_switching_force_x = 0.0  
+
+    if (dir_force_y > 0) & (cur_force_y > target_force_y[1]):
+        dir_force_y = dir_force_y * -1
+        target_force_y = limit_force_y + np.random.rand() * limit_range_force_y # randomly change target
+        vel_force_y = np.random.uniform(vel_limit_force_y[0], vel_limit_force_y[1]) # randomly change velocity
+        after_switching_force_y = 0.0  
+    if (dir_force_y < 0) & (cur_force_y < target_force_y[0]):
+        dir_force_y = dir_force_y * -1
+        target_force_y = limit_force_y + np.random.rand() * limit_range_force_y # randomly change target
+        vel_force_y = np.random.uniform(vel_limit_force_y[0], vel_limit_force_y[1]) # randomly change velocity
+        after_switching_force_y = 0.0  
+        
+    if (dir_force_z > 0) & (cur_force_z > target_force_z[1]):
+        dir_force_z = dir_force_z * -1
+        target_force_z = limit_force_z + np.random.rand() * limit_range_force_z # randomly change target
+        vel_force_z = np.random.uniform(vel_limit_force_z[0], vel_limit_force_z[1]) # randomly change velocity
+        after_switching_force_z = 0.0  
+    if (dir_force_z < 0) & (cur_force_z < target_force_z[0]):
+        dir_force_z = dir_force_z * -1
+        target_force_z = limit_force_z + np.random.rand() * limit_range_force_z # randomly change target
+        vel_force_z = np.random.uniform(vel_limit_force_z[0], vel_limit_force_z[1]) # randomly change velocity
+        after_switching_force_z = 0.0  
+
+            
+    cmd_force = np.zeros((3))
+    cmd_force[0] = cur_force_x + dir_force_x * vel_force_x *1e-3
+    cmd_force[1] = cur_force_y + dir_force_y * vel_force_y *1e-3
+    cmd_force[2] = cur_force_z + dir_force_z * vel_force_z *1e-3
+
+    force_msg = sensor_msgs.msg.JointState()
+    force_msg.position[:] = cmd_force
+    force_cmd_publisher.publish(force_msg)
+
+
+    cur_force_x = cmd_force[0]
+    cur_force_y = cmd_force[1]
+    cur_force_z = cmd_force[2]
+
+    after_switching_force_x += 1
+    #print(after_switching_force_x)
+    after_switching_force_y += 1
+    after_switching_force_z += 1
+
+#-----------------------------------------------------------------------------------------------------------------------------------------------------
+        
+    
+    
+
+target_jpos = np.zeros((16))
+target_jpos[1] = 30 * Deg2Rad
+target_jpos[2] = 75 * Deg2Rad
+target_jpos[3] = 0.300
+r2py_ctl.go_to_jr(target_jpos = target_jpos)
+r2py_ctl.pub_state_command('pause')
---
+++ python_controller/run_r2_trajectory_follow_with_force.py
@@ -0,0 +1,180 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 10 15:44:22 2022
+@author: 75678
+"""
+import sys
+import time
+import rospy
+import numpy as np
+import raven_py_controller
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+import sensor_msgs.msg
+
+smooth_factor = 100 # [IMPT]: defult 100, Must be positive. This is the factor to prevent the control command to be unstable. A larger factor could result in more smooth control commands, but the accuracy of following can be also decreased.
+max_step_dis = 10 # [IMPT]: defult 50, Must be positive, this is the max distance when finding the nearest point in the target trajectory
+min_step_dis = 1 # [IMPT]: defult 20, Must be positive, this is the min distance when finding the nearest point in the target trajectory
+min_step_thd = 10
+min_step_factor = 0.1 # [IMPT]: defult 0.1, Must be in (0,1),
+spd_factor = 2.0 # [IMPT]: defult 2.0, Must be positive
+
+show_plot = False # If true, a sub-realtime plot will be shown when running
+
+trajctory_file = 'trajectory/zigzag_traj_dir_yz_0.25.csv'
+
+Deg2Rad = np.pi / 180.0
+Rad2Deg = 180.0 / np.pi
+
+intv_record = 0.1
+intv_plot = 2
+
+time_last_record = 0
+time_last_plot = 0
+
+limit_joint_1 = np.array([15.0, 70.0]) * Deg2Rad
+limit_joint_2 = np.array([50.0, 115.0]) * Deg2Rad # should  be 115 deg
+limit_joint_3 = np.array([0.32, 0.43])
+
+velocity_joint_1 = 2.182 * Deg2Rad # max degree/s
+velocity_joint_2 = 3.0 * Deg2Rad # max degree/s
+velocity_joint_3 = 0.005 # max m/s
+#velocity_joint_1 = 3.0 * Deg2Rad # max degree/s
+#velocity_joint_2 = 3.0 * Deg2Rad # max degree/s
+#velocity_joint_3 = 0.01 # max m/s
+
+traj = np.loadtxt(trajctory_file,  delimiter=',')
+shape_traj = traj.shape
+
+#traj_len = shape_traj[0]
+
+# check if the traj exceed joint limits
+if np.min(traj[:,0]) < limit_joint_1[0]:
+    print('Joint 1 trajectory exceed min: ' + str(np.min(traj[:,0])*Rad2Deg) + ' Deg')
+    sys.exit('Exiting')
+if np.max(traj[:,0]) > limit_joint_1[1]:
+    print('Joint 1 trajectory exceed max: ' + str(np.max(traj[:,0])*Rad2Deg) + ' Deg')
+    sys.exit('Exiting')
+if np.min(traj[:,1]) < limit_joint_2[0]:
+    print('Joint 2 trajectory exceed min: ' + str(np.min(traj[:,1])*Rad2Deg) + ' Deg')
+    sys.exit('Exiting')
+if np.max(traj[:,1]) > limit_joint_2[1]:
+    print('Joint 2 trajectory exceed max: ' + str(np.max(traj[:,1])*Rad2Deg) + ' Deg')
+    sys.exit('Exiting')
+if np.min(traj[:,2]) < limit_joint_3[0]:
+    print('Joint 3 trajectory exceed min: ' + str(np.min(traj[:,2])) + ' m')
+    sys.exit('Exiting')
+if np.max(traj[:,2]) > limit_joint_3[1]:
+    print('Joint 3 trajectory exceed max: ' + str(np.max(traj[:,2])) + ' m')
+    sys.exit('Exiting')
+
+tail = np.ones((smooth_factor + 1, 3))
+traj = np.vstack((traj, tail))
+
+rospy.init_node('raven_trajectory_follower_with_force', anonymous=True)
+r2py_ctl = raven_py_controller.raven2_py_controller(name_space = ' ', robot_name = 'arm1', grasper_name = 'grasp1')
+force_cmd_publisher = rospy.Publisher('force_cmd', sensor_msgs.msg.JointState, latch = True, queue_size = 1)  # create torque cmd publisher
+r2py_ctl.pub_state_command('resume')
+
+rec_jpos = np.zeros((1,3))
+
+# first, go to start point
+print('Going to the start point of the trajectory')
+target_jpos = np.zeros((16))
+target_jpos[1] = traj[0,0]
+target_jpos[2] = traj[0,1]
+target_jpos[3] = traj[0,2]
+r2py_ctl.go_to_jr(target_jpos = target_jpos)
+print('Reach the start point, now follow the trajectory')
+
+start_time = time.time()
+idx_pre = 0
+moving = True
+avr_step_inc = 5
+min_step_cnt = 0
+while moving == True:
+    pos_cur = np.array([r2py_ctl.measured_jpos[0],r2py_ctl.measured_jpos[1],r2py_ctl.measured_jpos[2]])
+    idx = np.argmin(np.square(traj[:,0]-pos_cur[0]) + np.square(traj[:,1]-pos_cur[1]) + np.square(traj[:,2]-pos_cur[2]))
+    
+    if idx >= (shape_traj[0] - 200):
+        end_time = time.time()-start_time
+
+        for i in range(0,2000):
+            cmd = np.zeros((16))
+            r2py_ctl.pub_jr_command(cmd * 1e-3) 
+
+        target_jpos = np.zeros((16))
+        target_jpos[1] = 30 * Deg2Rad
+        target_jpos[2] = 75 * Deg2Rad
+        target_jpos[3] = 0.300
+        r2py_ctl.go_to_jr(target_jpos = target_jpos)        
+        r2py_ctl.pub_state_command('pause')
+        print('Trajectory following finished, time used(sec):')
+        print(end_time)
+        sys.exit('Trajectory finished, exiting')
+    avr_step_inc = (1-min_step_factor)*avr_step_inc + min_step_factor*(idx - idx_pre)
+    if (idx - idx_pre) >= max_step_dis:
+        #idx = idx + max_step_dis
+        idx = idx_pre + max_step_dis
+    #elif avr_step_inc <= 0.5:
+        #idx = idx_pre + 1
+    elif (idx - idx_pre) <= min_step_dis:
+        min_step_cnt = min_step_cnt+1
+        idx = idx_pre
+        if min_step_cnt >= min_step_thd:
+            idx = idx_pre + min_step_dis
+            min_step_cnt = 0
+    
+    pos_tar = traj[idx+smooth_factor]
+    
+    cmd = np.zeros((16))
+    cmd[1] = np.clip(spd_factor*(pos_tar[0] - pos_cur[0]), -velocity_joint_1, velocity_joint_1)
+    cmd[2] = np.clip(spd_factor*(pos_tar[1] - pos_cur[1]), -velocity_joint_2, velocity_joint_2)
+    cmd[3] = np.clip(spd_factor*(pos_tar[2] - pos_cur[2]), -velocity_joint_3, velocity_joint_3)
+    print('------------------------------')
+    print(idx)
+    #print(traj_len)
+    print(cmd[1] * Rad2Deg)
+    print(cmd[2] * Rad2Deg)
+    print(cmd[3])
+    if np.abs(cmd[1] * Rad2Deg) < 0.2:
+        cmd[1] = 0
+
+    if np.abs(cmd[2] * Rad2Deg) < 0.2:
+        cmd[2] = 0
+
+    if np.abs(cmd[3]) < 0.0005:
+        cmd[3] = 0
+
+    #print(cmd)
+    r2py_ctl.pub_jr_command(cmd * 1e-3)     
+    
+    idx_pre = idx
+
+    # compute and publish force command-----------------------
+    force_msg = sensor_msgs.msg.JointState()
+    force_msg.position[:] = np.zeros(3)
+    force_msg.position[1] = 4.936251773434784* pos_cur[1] -7.107692307692307  # this is to match range [50, 115] degree to range [-2.8, +2.8] N froce
+    force_cmd_publisher.publish(force_msg)
+
+    #---------------------------------------------------------
+
+    if show_plot == True:
+        if time.time()-time_last_record > intv_record:
+            rec_jpos = np.append(rec_jpos, np.array([pos_cur]), axis = 0)
+            time_last_record = time.time()
+        if time.time()-time_last_plot > intv_plot:
+            fig1 = plt.figure(1)
+            fig1.clf()
+            ax1 = fig1.add_subplot(111, projection='3d')
+            #ax1 = fig1.gca(projection='3d')
+            ax1.set_xlabel('joint 1(Deg)')
+            ax1.set_ylabel('joint 2(Deg)')
+            ax1.set_zlabel('joint 3(m)')
+            ax1.plot(rec_jpos[1:,0]*Rad2Deg, rec_jpos[1:,1]*Rad2Deg, rec_jpos[1:,2], c='b')
+            ax1.plot(traj[:,0]*Rad2Deg, traj[:,1]*Rad2Deg, traj[1:,2], c='r')
+            ax1.scatter(rec_jpos[-1,1]*Rad2Deg, rec_jpos[-1,2]*Rad2Deg, rec_jpos[-1,3], label=str("%.4f" % (rec_jpos[-1,1]*Rad2Deg)) + ', ' + str("%.4f" % (rec_jpos[-1,2]*Rad2Deg)) + ', ' + str("%.4f" % (rec_jpos[-1,3])), c='y')         
+            ax1.legend()
+            plt.title('3D traj in joint space')
+            plt.show(block = False)
+            time_last_plot = time.time()
